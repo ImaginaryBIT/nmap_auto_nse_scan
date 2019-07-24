0d03eb95d46c12dbe4caf6c5717591c26ca821e5
ImaginaryBIT <32798440+ImaginaryBIT@users.noreply.github.com>
2019-07-24 22:26:31 +0800

    fix bug

--- nse.sh
+++ nse.sh
@@ -5,11 +5,12 @@
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
+NC='\033[0m'
 
-echo "${YELLOW}This is a nmap nse scanning automation script"
-echo "${YELLOW}Scope of testing: all TCP port and top 1000 UDP port"
-echo "${GREEN}Example: nse 192.168.0.108"
-echo "${RED}Only work on single IP address"
+echo -e "${YELLOW}This is a nmap nse scanning automation script"
+echo -e "${YELLOW}Scope of testing: all TCP port and top 1000 UDP port"
+echo -e "${GREEN}Example: nse 192.168.0.108"
+echo -e "${RED}Only work on single IP address"
 
 mkdir nmap
 dir=$(pwd)/nmap
@@ -20,24 +21,24 @@ touch $dir/nmap_nse_scan_result.log
 
 ### phase 1: get all open ports
 
-echo "${GREEN}===> scanning all the port"
+echo -e "${GREEN}===> scanning all the port${NC}"
 nmap -T5 -Pn --open -n --min-rate=1000 --max-retries=2 -p- -oX $dir/TCP_open_port.xml $1
 nmap -T5 -Pn --open -n --min-rate=1000 --max-retries=2 -sU -oX $dir/UDP_open_port.xml $1
-echo "${GREEN}===> scanning on all the port done"
+echo -e "${GREEN}===> scanning on all the port done${NC}"
 
 ### phase 2: nse scan on open ports
 
-echo "${GREEN}### NSE scanning started ###"
+echo -e "${GREEN}### NSE scanning started ###${NC}"
 cat $dir/TCP_open_port.xml|grep "service name"| while read line
 do
 protocol=$(echo $line | cut -d'"' -f 12)
-if(ls dir_nmap | grep -q "^$protocol")
+if(ls $dir_nmap | grep -q "^$protocol")
 then
 	port=$(echo $line | cut -d'"' -f 4)
-	echo "${YELLOW}===> scanning on port $port $protocol"
-	nmap -Pn -sV -T4 -p$port --script="$protocol-*" -n $1 >> nmap_nse_scan_result.log
+	echo -e "${YELLOW}===> scanning on port $port $protocol${NC}"
+	nmap -Pn -sV -T4 -p$port --script="$protocol-* and not *-brute" -n $1 >> nmap_nse_scan_result.log
 else
-	echo "${RED}===> NSE for $protocol not found! continue with default script scan"
+	echo -e "${RED}===> NSE for $protocol not found! continue with default script scan${NC}"
 	nmap -Pn -sV -T4 -p$port -sC -n $1 >> nmap_nse_scan_result.log
 fi
 done
@@ -45,15 +46,15 @@ done
 cat $dir/UDP_open_port.xml|grep "service name"| while read line
 do
 protocol=$(echo $line | cut -d'"' -f 12)
-if(ls dir_nmap | grep -q "^$protocol")
+if(ls $dir_nmap | grep -q "^$protocol")
 then
 	port=$(echo $line | cut -d'"' -f 4)
-	echo "${YELLOW}===> scanning on port $port $protocol"
-	nmap -Pn -sU -sV -T4 -p$port --script="$protocol-*" -n $1 >> nmap_nse_scan_result.log
+	echo -e "${YELLOW}===> scanning on port $port $protocol${NC}"
+	nmap -Pn -sU -sV -T4 -p$port --script="$protocol-* and not *-brute" -n $1 >> nmap_nse_scan_result.log
 else
-	echo "${RED}===> NSE for $protocol not found! continue with default script scan"
+	echo -e "${RED}===> NSE for $protocol not found! continue with default script scan${NC}"
 	nmap -Pn -sU -sV -T4 -p$port -sC -n $1 >> nmap_nse_scan_result.log
 fi
 done
-echo "${GREEN}### Scanning finished ###"
+echo "${GREEN}### Scanning finished ###${NC}"
  
